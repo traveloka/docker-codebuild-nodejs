70a91f9068f27f707755e4de3965e5e76e191e84
Abi Hafshin Alfarouq <abi.alfarouq@traveloka.com>
2018-05-04 17:51:03 +0700

    Add circle ci config

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+IMAGE_NAME="${1}"
+DOCKER=$(which docker)
+DOCKER_RUN="${DOCKER} run --rm -t ${IMAGE_NAME}"
+
+echo -n "Check nodejs: "
+${DOCKER_RUN} node --version && echo -e "OK\n"
+
+echo -n "Check yarn: "
+${DOCKER_RUN} yarn --version && echo -e "OK\n"
+
+echo -n "Check chrome browser: "
+${DOCKER_RUN} /opt/chrome-linux/chrome --version && echo -e "OK\n"
+
+echo -n "Check aws cli: "
+${DOCKER_RUN} aws --version && echo -e "OK\n"
+
+echo -n "Check Docker client: "
+${DOCKER_RUN} docker --version && echo -e "OK\n"
+
+echo -n "Check Docker server: "
+${DOCKER_RUN} docker-containerd --version && echo -e "OK\n"
+
+
